d198475ab6e9c6242f594c808dbcc4db7c4b903d
Councy <kauatadeucel@gmail.com>
2023-05-25 15:28:40 -0300

    Aula 25/05

---
+++ Gestão Estudantes Kauã/Login_Form.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gestão_Estudantes_Kauã
+{
+    public partial class Login_Form : Form
+    {
+        public Login_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Form_Load(object sender, EventArgs e)
+        {
+            // Definir a imagem de login pelo código.
+            pictureBox1.Image = Image.FromFile("../../imagens/avatarGIF.gif");
+        }
+    }
+}
