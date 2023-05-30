5cf8c05d8f39db40af12bfd5ba9f382c6507a6a7
Councy <kauatadeucel@gmail.com>
2023-05-30 17:21:12 -0300

    Aula 30/05
    Criado a conexão com o banco de dados.

---
+++ Gestão Estudantes Kauã/MEU_BD.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Gestão_Estudantes_Kauã
+{
+    // A classe do nosso banco de dados.
+    internal class MEU_BD
+    {
+        private MySqlConnection conexao = new MySqlConnection("datasource=localhost; username=root; password=;database=sga_estudantes_bd_t4");
+    }
+}
